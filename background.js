55cb69b3dbac2b32a936083c47bd67556cd50cff
Bradley <btsutedja@gmail.com>
2022-05-17 09:46:05 -0700

    removed background.js

--- background.js
+++
@@ -1,3 +0,0 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
-        chrome.tabs.executeScript(null, { file: "content-script.js" });
-});
--- content-script.js
+++ content-script.js
@@ -3,12 +3,10 @@ document.addEventListener('yt-navigate-start', process);
 if (document.body) process();
 else document.addEventListener('DOMContentLoaded', process);
 
-function process() {
+function process() { 
     if (!location.pathname.startsWith('/shorts')) {
         return;
     }
-    var curloc = document.location.href.split('shorts/'); 
-    var urlEnding = curloc[1]; 
-    curloc = 'https://www.youtube.com/watch?v='+urlEnding;
-    document.location.href = curloc;
+    let host = 'https://www.youtube.com/watch?v='
+    document.location.href = host + document.location.href.split('shorts/')[1]
 }
